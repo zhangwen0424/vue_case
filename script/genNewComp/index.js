758db69cd679675e45562cfab8207b06b555e9d6
mornki <1048515550@qq.com>
2022-08-02 09:47:57 +0800

    生成组件

---
+++ script/genNewComp/index.js
@@ -0,0 +1,20 @@
+const infoCollector = require("./infoCollector");
+const tplReplacer = require("./tplReplacer");
+
+// 生成新组件并更新组件信息
+async function run() {
+  const meta = await infoCollector();
+  tplReplacer(meta);
+}
+
+// 根据组件目录生成组件信息
+async function init() {
+  tplReplacer();
+}
+
+let argvs = process.argv.slice(2);
+if (argvs[0] == "init") {
+  init(); // 不生成新组件
+} else {
+  run(); //生成新组件
+}
